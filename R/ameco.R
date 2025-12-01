7b8f8e8cd6397fac11b2a83559abb6a0c27f14ad
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2025-12-01 09:02:35 +0100

    update url 11 2025

--- R/ameco.R
+++ R/ameco.R
@@ -1,15 +1,17 @@
 #' AMECO database
 #'
-#' Télécharge et renvoie le fichier complet d'AMECO pour le vintage demandé. Les vintages disponibles sont ceux sur le site de la Commission Eruopéen de 2011 à 2025, avec deux prévisions par an (printemps et automne) et une préivison à l'hiver 2015.
-#' @param version (default 5/2025) version of AMECO (either 5/xxxx or spring xxxx for spring forecast or 11/xxxx autumn xxxx for autumn forecast)
+#' Télécharge et renvoie le fichier complet d'AMECO pour le vintage demandé.
+#'
+#' Les vintages disponibles sont ceux sur le site de la Commission Eruopéen de 2011 à 2025,
+#' avec deux prévisions par an (printemps et automne) et une préivison à l'hiver 2015.
+#'
+#' @param version (default 11/2025) version of AMECO (either 5/xxxx or spring xxxx for spring forecast or 11/xxxx autumn xxxx for autumn forecast)
 #' @param reset (download even if cache is present)
 #'
 #' @returns a tibble withn AMECO
 #' @export
-#'
-#' @examples
 
-ameco <- function(version = "5/2025", reset = FALSE) {
+ameco <- function(version = "11/2025", reset = FALSE) {
   return(ameco_xx(version, reset)$data)
 }
 
@@ -21,30 +23,36 @@ ameco <- function(version = "5/2025", reset = FALSE) {
 #'
 #' @returns a tibble withn AMECO codes
 #' @export
-#'
-#' @examples
 
 ameco_codes <- function(version = "5/2025", reset = FALSE) {
   return(ameco_xx(version, reset)$codes)
 }
 
 ameco_xx <- function(version = "5/2025", reset = FALSE) {
-    version <- version_ameco(version)
-    ameco_url <- url_ameco(version)
+  version <- version_ameco(version)
+  ameco_url <- url_ameco(version)
 
-    freset <- !file.exists(".ameco/ameco_{version}.qs2" |> glue::glue()) |
-      !file.exists(".ameco/ameco_countries_{version}.qs2" |> glue::glue()) |
-      !file.exists(".ameco/ameco_codes_{version}.qs2" |> glue::glue())
-    if (freset | reset) {
-      dt <- download_ameco(url = ameco_url)
-      qs2::qs_save(dt$data, file = ".ameco/ameco_{version}.qs2" |> glue::glue())
-      qs2::qs_save(dt$codes, file = ".ameco/ameco_codes_{version}.qs2" |> glue::glue())
-      qs2::qs_save(dt$countries, file = ".ameco/ameco_countries_{version}.qs2" |> glue::glue())
-    } else {
-      dt <- list()
-      dt$data <- qs2::qs_read(".ameco/ameco_{version}.qs2" |> glue::glue())
-      dt$codes <- qs2::qs_read(".ameco/ameco_codes_{version}.qs2" |> glue::glue())
-      dt$countries <- qs2::qs_read(".ameco/ameco_countries_{version}.qs2" |> glue::glue())
-    }
-    return(dt)
+  freset <- !file.exists(".ameco/ameco_{version}.qs2" |> glue::glue()) |
+    !file.exists(".ameco/ameco_countries_{version}.qs2" |> glue::glue()) |
+    !file.exists(".ameco/ameco_codes_{version}.qs2" |> glue::glue())
+  if (freset | reset) {
+    dt <- download_ameco(url = ameco_url)
+    qs2::qs_save(dt$data, file = ".ameco/ameco_{version}.qs2" |> glue::glue())
+    qs2::qs_save(
+      dt$codes,
+      file = ".ameco/ameco_codes_{version}.qs2" |> glue::glue()
+    )
+    qs2::qs_save(
+      dt$countries,
+      file = ".ameco/ameco_countries_{version}.qs2" |> glue::glue()
+    )
+  } else {
+    dt <- list()
+    dt$data <- qs2::qs_read(".ameco/ameco_{version}.qs2" |> glue::glue())
+    dt$codes <- qs2::qs_read(".ameco/ameco_codes_{version}.qs2" |> glue::glue())
+    dt$countries <- qs2::qs_read(
+      ".ameco/ameco_countries_{version}.qs2" |> glue::glue()
+    )
   }
+  return(dt)
+}
--- R/url_ameco.R
+++ R/url_ameco.R
@@ -8,9 +8,9 @@
 #' @examples
 #' url_ameco("5/2025")
 
-url_ameco <- function(version = "52025") {
+url_ameco <- function(version = "11025") {
   ameco_url <- c(
-    "52021"  = "https://economy-finance.ec.europa.eu/document/download/17372dca-211c-42b6-9a1d-89559b985d23_en?filename=ameco_spring2021.zip",
+    "52021" = "https://economy-finance.ec.europa.eu/document/download/17372dca-211c-42b6-9a1d-89559b985d23_en?filename=ameco_spring2021.zip",
     "112021" = "https://ec.europa.eu/economy_finance/db_indicators/ameco/documents/ameco_autumn2021.zip",
     "52022" = "https://economy-finance.ec.europa.eu/document/download/c5be576e-fbea-45f3-98a5-3cf59d16d70a_en?filename=ameco_spring2022.zip",
     "112022" = "https://economy-finance.ec.europa.eu/document/download/b16e473a-04ef-4ec8-b2f8-1e0c30a39d3c_en?filename=ameco_autumn2022.zip",
@@ -18,7 +18,8 @@ url_ameco <- function(version = "52025") {
     "112023" = "https://economy-finance.ec.europa.eu/document/download/6a48b097-bd09-496b-908f-404d0e85cff8_en?filename=AMECO_2023_Autumn_Revised.zip",
     "52024" = "https://economy-finance.ec.europa.eu/document/download/ad8d7bd0-c4d9-4860-b597-bb613408d6bd_en?filename=AMECO_2024_Spring_final.zip",
     "112024" = "https://economy-finance.ec.europa.eu/document/download/9c2630c3-6c25-456d-b484-6b05c202e8ce_en?filename=ameco0.zip",
-    "52025" = "https://ec.europa.eu/economy_finance/db_indicators/ameco/documents/ameco0.zip"
+    "52025" = "https://economy-finance.ec.europa.eu/document/download/b4b375e7-80a6-4581-b8fd-9c207790cd01_en?filename=ameco0.zip",
+    "112025" = "https://ec.europa.eu/economy_finance/db_indicators/ameco/documents/ameco0.zip"
   )
   return(ameco_url[[version]])
 }
